68c60c0947a46681557891822a8c6c3d8859819b
John Wozniak <jjwozn0977@gmail.com>
2016-05-11 21:17:51 -0400

    added the rest of the CRUD model, and styling

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,12 +13,32 @@ class PostsController < ApplicationController
     if @post.save
       redirect_to @post
     else
-      render new
+      render 'new'
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(params[:post].permit(:title, :body))
+      redirect_to @post
+    else
+      render 'edit'
     end
   end
 
   def show
     @post = Post.find(params[:id])
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to root_path
   end
 
   private
